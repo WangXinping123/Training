5a01d5090ccdb86b1b3dd8dd491d9c8e02532b72
sheepsbrother <39666314+sheepsbrother@users.noreply.github.com>
2019-12-05 19:17:21 +0800

    Add files via upload

---
+++ shouye/NewTest.java
@@ -0,0 +1,11 @@
+package com.webtest.shouye;
+
+import org.testng.annotations.Test;
+
+import com.webtest.core.BaseTest;
+
+public class NewTest  extends BaseTest{
+  @Test (description="设置为默认地址")
+  public void f() {
+  }
+}
