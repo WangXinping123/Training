f55c5fe15db0e60ba28cb7998e02388f504c0a52
WangXinping123 <2294591295@qq.com>
2019-12-05 17:48:38 +0800

    14周代码

--- 王欣平/src/shoppingCart/TestAddCart.java
+++ 王欣平/src/shoppingCart/TestAddCart.java
@@ -1,10 +1,13 @@
 package shoppingCart;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Sleeper;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
@@ -15,67 +18,44 @@ import org.testng.internal.remote.SlavePool;
 import com.webtest.core.BaseTest;
 import com.webtest.core.ApiListener;
 import com.webtest.dataprovider.ExcelDataProvider;
+import com.webtest.utils.ReadProperties;
 
-@Listeners({ ApiListener.class })
+
+/**
+ * 项目名称：TPSHOP   
+ * 类名称：TestAddCart   
+ * 类描述： 多次添加不同属性的相同商品
+ * @author wangxinping 
+ * 创建时间：2019年11月29日 下午4:26:14   
+ * @version  
+ */
 public class TestAddCart extends BaseTest {
 
 	public int i = 0;
-	
+	public int a = 0;
+
 	@Test(priority = 0, description = "登录")
 	public void testLogin() throws Exception {
-		// 打开网页
-		webtest.open("http://localhost:8036/index.php");
+		LoginMethod();
 		Thread.sleep(1000);
-		// 点击“登录”链接
-		webtest.click("xpath=//a[@href='/index.php/Home/user/login.html']");
-
-		webtest.type("name=username", "13800138006");
-		webtest.type("name=password", "123456");
-		webtest.type("name=verify_code", "1111");
-		webtest.click("name=sbtbutton");
-		Thread.sleep(1000);
-		Assert.assertTrue(getDriver().getPageSource().contains("安全退出"));
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("退出"));
 	}
 
 	@DataProvider(name = "excel")
 	public Object[][] getExcelDada() throws IOException {
-		return new ExcelDataProvider().getTestDataByExcel("E:/data/property.xlsx", "Sheet1");
+		return new ExcelDataProvider().getTestDataByExcel(
+				ReadProperties.getPropertyValue("data_path")+"property.xlsx", "Sheet1");
 	}
 
-	@Test(description = "选择商品")
-	public void test_choose() throws Exception {
-		// 选择商品
-		JavascriptExecutor js = (JavascriptExecutor) getDriver();
-		webtest.click("xpath=//a[contains(text(),'返回商城首页')]");
-		Thread.sleep(500);
-		webtest.click("xpath=//a[contains(text(),'手机城')]");
-		Thread.sleep(500);
-
-		// 跳转到商品列表
-		webtest.getWindow(1);
-		System.out.println(webtest.getTitle());
-
-		WebElement ele = getDriver().findElement(By.xpath("//a[@href='/index.php/Home/Goods/goodsInfo/id/1.html']"));
-		// 选择商品
-		js.executeScript("arguments[0].click();", ele);
-		Thread.sleep(500);
-		// 跳转到商品详情页
-		webtest.getWindow(2);
-		System.out.println(webtest.getTitle());
-		Thread.sleep(200);
-	}
-
-	@Test(description="打开商品详情")
-	public void test_Open1() throws Exception{
+	@Test(priority=2,description = "打开商品详情")
+	public void test_Open1() throws Exception {
 		webtest.open("http://localhost:8036/index.php/Home/Goods/goodsInfo/id/1.html");
-		Thread.sleep(200);
+		Thread.sleep(800);
 	}
-	
+
 	// 多次添加同一商品不同属性
-	@Test(dataProvider = "excel", dependsOnMethods="test_Open1",
-			description = "登录，同一商品不同属性添加多次")
-	public void test_03_AddCartDifferentProperty(String version, String color, String type)
-			throws Exception{
+	@Test(priority=3,dataProvider = "excel", description = "登录，同一商品不同属性添加多次")
+	public void test_03_AddCartDifferentProperty(String version, String color, String type) throws Exception {
 		i++;// iframe id每次都加1
 		// 选择版本
 		webtest.click("xpath=//a[contains(text(),'" + version + "')]");
@@ -84,24 +64,134 @@ public class TestAddCart extends BaseTest {
 		// 套餐类型
 		webtest.click("xpath=//a[contains(text(),'" + type + "')]");
 		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
-		Thread.sleep(500);
-		WebElement frame = getDriver().findElement(By.id("layui-layer-iframe" + i));
-		getDriver().switchTo().frame(frame);
-		Assert.assertTrue(getDriver().getPageSource().contains("添加成功"));
-		webtest.click("xpath=//a[contains(text(),'继续购物')]");
 		Thread.sleep(200);
-		getDriver().switchTo().defaultContent();
+		webtest.enterFrame("layui-layer-iframe" + i);
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("添加成功"));
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(100);
+		webtest.leaveFrame();
 		Thread.sleep(1000);
 	}
 	
-	//加入购物车，点击 去购物车结算
-	@Test
-	public void test_04_chooseAccount(){
-		String url="/Home/Goods/goodsInfo/id/5.html";
-		webtest.open(url);
-		
+	@Test(priority = 5, description = "打开商品详情25")
+	public void test_Open2() throws Exception {
+		webtest.open("http://localhost:8036/index.php/Home/Goods/goodsInfo/id/25.html");
+		Thread.sleep(200);
 	}
-	
-	
+
+	@Test(priority = 7, description = "点击一次商品数量右侧的+号", dependsOnMethods = "test_Open2")
+	public void test_04_addGoodsNuByButton1() throws Exception {
+		// action.moveByOffset(100, 200).release()
+		a++;// iframe id每次都加1
+		// 选择数量
+		webtest.click("xpath=//div[@class=minus-plus]/a[@class=add]");
+		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
+		Thread.sleep(200);
+		webtest.enterFrame("layui-layer-iframe" + a);
+
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("添加成功"));
+
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(200);
+		webtest.leaveFrame();
+		Thread.sleep(1000);
+	}
+
+	@Test(priority = 10, description = "点击多次商品数量右侧的+号")
+	public void test_05_addGoodsNuByButton3() throws Exception {
+		System.out.println(webtest.getTitle());
+		// action.moveByOffset(100, 200).release()
+		a++;// iframe id每次都加1
+		int m;
+		for (m = 0; m < 5; m++) {
+			webtest.click("class=add");
+		}
+		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
+		Thread.sleep(200);
+		webtest.enterFrame("layui-layer-iframe" + a);
+
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("添加成功"));
+
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(200);
+		webtest.leaveFrame();
+		Thread.sleep(1000);
+	}
+
+	@Test(priority = 15, description = "点击商品数量左侧的-号")
+	public void test_06_addGoodsNuByButton2() throws Exception {
+		System.out.println(webtest.getTitle());
+		a++;// iframe id每次都加1
+		int m;
+		for (m = 0; m < 5; m++) {
+			webtest.click("class=mins");
+		}
+		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
+		Thread.sleep(200);
+		webtest.enterFrame("layui-layer-iframe" + a);
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("添加成功"));
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(200);
+		webtest.leaveFrame();
+		Thread.sleep(1000);
+	}
+
+	// 登录，添加商品数量为-1
+	@Test(priority = 20, description = "选择商品数量: -1 失败")
+	public void test_08_addGoodsNumFail01() throws Exception {
+		a++;// iframe id每次都加1
+		// 选择数量
+		webtest.type("id=number", "-1");
+		webtest.tapClick();
+		// webtest.tapClick();
+		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
+		Thread.sleep(200);
+		webtest.enterFrame("layui-layer-iframe" + a);
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("添加失败"));
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(200);
+		webtest.leaveFrame();
+		Thread.sleep(2000);
+	}
+
+	// 登录，添加商品数量为 0
+	@Test(priority = 25, description = "选择商品数量 0  失败")
+	public void test_10_addGoodsNumFail02() throws Exception {
+		a++;// iframe id每次都加1
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(200);
+		webtest.leaveFrame();
+		Thread.sleep(2000);
+		// 选择数量
+		webtest.type("id=number", "-1");
+		webtest.tapClick();
+		// webtest.tapClick();
+		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
+		Thread.sleep(2000);
+		webtest.enterFrame("layui-layer-iframe" + a);
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("添加失败"));
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(200);
+		webtest.leaveFrame();
+		Thread.sleep(2000);
+	}
+
+	// 登录，添加商品数量为 库存数+1
+	@Test(priority = 30, description = "选择商品数量  库存数+1 失败")
+	public void test_12_addGoodsNumFail03() throws Exception {
+		a++;// iframe id每次都加1
+		webtest.click("xpath=//a[contains(text(),'继续购物')]");
+		Thread.sleep(200);
+		webtest.leaveFrame();
+		Thread.sleep(1000);
+		// 选择数量
+		webtest.type("id=number", "100");
+		webtest.tapClick();
+		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
+		Thread.sleep(200);
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("商品库存不足"));
+		webtest.click("xpath=//a[contains(text(),'确定')]");
+		Thread.sleep(200);
+	}
 
 }
--- 王欣平/src/shoppingCart/TestCartWithoutLogin.java
+++ 王欣平/src/shoppingCart/TestCartWithoutLogin.java
@@ -1,5 +1,7 @@
 package shoppingCart;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -11,7 +13,15 @@ import org.testng.annotations.Test;
 import com.webtest.core.ApiListener;
 import com.webtest.core.BaseTest;
 
-@Listeners({ApiListener.class})
+/**
+ * 项目名称：TPSHOP   
+ * 类名称：TestCartWithoutLogin   
+ * 类描述： 不登录时，查看购物车，加入购物车
+ * @author wangxinping 
+ * 创建时间：2019年11月29日 下午4:27:26   
+ * @version  
+ */
+
 public class TestCartWithoutLogin extends BaseTest {
 
 	@Test(priority=0,description = "打开网站")
@@ -21,7 +31,7 @@ public class TestCartWithoutLogin extends BaseTest {
 	}
 
 	// 未登录，查看购物车
-	@Test(description = "未登录，查看购物车")
+	@Test(priority=1,description = "未登录，查看购物车")
 	public void test_01_ViewCart() throws Exception {
 		
 		if (getDriver().getPageSource().contains("安全退出")) {
@@ -31,26 +41,23 @@ public class TestCartWithoutLogin extends BaseTest {
 		}
 		webtest.click("xpath=//a[@href='/index.php/Home/Cart/index.html']/div");
 		Thread.sleep(200);
-		Assert.assertTrue(getDriver().getPageSource().contains("登录"));
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("登录"));
 		Thread.sleep(1000);
 	}
 
 	// 未登录，加入购物车
-	@Test(description = "未登录，加入购物车")
+	@Test(priority=2,description = "未登录，加入购物车")
 	public void test_02_AddCart() throws Exception {
 		if (getDriver().getPageSource().contains("安全退出")) {
 			// 退出登录
 			webtest.click("xpath=//a[contains(text(),'安全退出')]");
 			Thread.sleep(800);
 		}
-		
 		webtest.click("xpath=//a[contains(text(),'手机')]");
 		Thread.sleep(500);
-
 		// 跳转到 搜索列表
 		webtest.getWindow(1);
 		System.out.println(webtest.getTitle());
-
 		WebElement ele = getDriver().findElement(By.xpath("//a[@href='/index.php/Home/Goods/goodsInfo/id/1.html']"));
 		// 选择商品
 		JavascriptExecutor js = (JavascriptExecutor) getDriver();
@@ -63,18 +70,27 @@ public class TestCartWithoutLogin extends BaseTest {
 		// webtest.click(locator);
 		// 套餐类型
 		webtest.click("xpath=//a[contains(text(),'套餐二')]");
-
 		webtest.click("xpath=//a[contains(text(),'加入购物车')]");
 		Thread.sleep(500);
-
-		Assert.assertTrue(getDriver().getPageSource().contains("登录"));
-
-		/*WebElement frame = getDriver().findElement(By.id("layui-layer-iframe1"));
-		getDriver().switchTo().frame(frame);
-		webtest.click("xpath=//a[contains(text(),'继续购物')]");
-		Thread.sleep(500);
-		getDriver().switchTo().defaultContent();*/
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("登录"));
 		Thread.sleep(2000);
 	}
+	
+	@Test(priority = 4, description = "登录")
+	public void testLogin() throws Exception {
+		LoginMethod();
+		Thread.sleep(1000);
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("退出"));
+	}
+	
+	@Test(priority=5,description="点击购物车图标")
+	public void test_01_viewCart()throws Exception{
+		webtest.click("xpath=//a[contains(text(),'返回商城首页')]");
+		Thread.sleep(500);
+		webtest.click("xpath=//span[contains(text(),'购物车')]");
+		Thread.sleep(200);
+		AssertJUnit.assertTrue(getDriver().getPageSource().contains("全部商品"));
+	}
+	
 
 }
